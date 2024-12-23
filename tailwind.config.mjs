ad5966e576ca2fdb1ab573a63680a942bad54a21
RigelKukoy <cabayarigel1@gmail.com>
2024-12-23 08:00:13 +0800

    Fix build configuration for GitHub Pages

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,8 +4,7 @@ export default {
   './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
   './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  './node_modules/flowbite/**/*.js',
-  './node_modules/flowbite-react/**/*.js',
+  'node_modules/flowbite-react/lib/esm/**/*.js',
  ],
  theme: {
   extend: {
